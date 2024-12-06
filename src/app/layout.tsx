a1c18c2e896fe5276bab796d235ef1b31503bf01
wxydev002 <wxydev002@outlook.com>
2024-12-06 09:51:54 +0800

    [beta] scrollbar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import { PageLayout } from "./client";
 import "./global.css";
 import "react-circular-progressbar/dist/styles.css";
-import "flag-icons/css/flag-icons.min.css"
+import "flag-icons/css/flag-icons.min.css";
 
 export const metadata = {
   title: "EnReach Dashboard",
@@ -19,7 +19,7 @@ const popins = Poppins({
 });
 const helvetica = localFont({
   variable: "--HelveticaNeue",
-  
+
   src: [
     {
       path: "./HelveticaNeueBold.otf",
@@ -33,6 +33,9 @@ const fonts = [popins, helvetica];
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en" className={cn("dark", fonts.map((f) => f.variable).join(" "), "font-Poppins")}>
+      <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, user-scalable=0" />
+      </head>
       <body>
         <PageLayout>{children}</PageLayout>
       </body>
