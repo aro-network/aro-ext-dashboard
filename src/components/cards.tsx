d95f90cb1ce77963eaf126ffb6565e13cacf27df
wxydev002 <wxydev002@outlook.com>
2024-12-17 16:58:05 +0800

    [beta] fix radius

--- src/components/cards.tsx
+++ src/components/cards.tsx
@@ -10,7 +10,7 @@ export function IconCard({ icon, className, iconSize = 30, tit, content }: { ico
   const leftSizeRem = pxToRem(120 - iconSize);
   const pl = pxToRem((30 - iconSize) / 2);
   return (
-    <Card className={cn("bg-gray-1 bg-no-repeat flex flex-col p-[1.75rem] gap-[2.8125rem] relative", className)}>
+    <Card className={cn("bg-gray-1 bg-no-repeat rounded-3xl flex flex-col p-[1.75rem] gap-[2.8125rem] relative", className)}>
       <SVGS.SvgBgIconCard className="absolute left-0 top-0 text-[6.25rem] z-0" />
       <div className="flex items-center whitespace-nowrap" style={{ height: sizeRem }}>
         <div
@@ -42,7 +42,7 @@ export function InnerIconCard({ icon, className, iconSize = 30, children }: Prop
   const leftSizeRem = pxToRem(120 - iconSize);
   const pl = pxToRem((30 - iconSize) / 2);
   return (
-    <Card className={cn("bg-[#F5F5F514] bg-no-repeat flex flex-row p-[1.875rem] relative", className)}>
+    <Card className={cn("bg-[#F5F5F514] bg-no-repeat rounded-3xl flex flex-row p-[1.875rem] relative", className)}>
       <SVGS.SvgBgIconCard className="absolute left-0 top-0 text-[6.6rem] z-0" />
       <div
         className="shrink-0"
@@ -63,7 +63,7 @@ export function InnerIconCard({ icon, className, iconSize = 30, children }: Prop
 
 export function TitCard(p: PropsWithChildren & { tit?: string; right?: ReactNode; className?: string }) {
   return (
-    <Card className={cn("bg-gray-1 bg-no-repeat flex p-6 gap-6", p.className)}>
+    <Card className={cn("bg-gray-1 bg-no-repeat rounded-3xl flex p-6 gap-6", p.className)}>
       {p.tit && (
         <div className="flex items-center justify-between">
           <span className="text-base font-semibold font-Alexandria">{p.tit}</span>
@@ -78,6 +78,6 @@ export function TitCard(p: PropsWithChildren & { tit?: string; right?: ReactNode
 export function BgCard(p: PropsWithChildren & { className?: string, wrapClassName?: string }) {
   return <div className={cn("relative overflow-visible flip_item", p.wrapClassName)}>
     <div style={{ backgroundSize: '100% 100%' }} className="bg-s1 bg-no-repeat animate-pulse w-[calc(100%_+_2.5rem)] h-[calc(100%_+_2.5rem)] absolute -left-5 -top-5 z-0" />
-    <Card className={cn("bg-[#2E2E2E] border border-[#007AFF] flex flex-col justify-start items-center p-6 gap-6 w-full h-full", p.className)}>{p.children}</Card>
+    <Card className={cn("bg-[#2E2E2E] border border-[#007AFF] flex flex-col justify-start items-center p-6 gap-6 w-full h-full rounded-3xl", p.className)}>{p.children}</Card>
   </div>
 }
